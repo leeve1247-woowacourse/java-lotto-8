71101bd077b31c50bc177bed7f55294bd6ec280c
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-29 05:54:07 +0900

    refactor(dto): 로또 게임 dto 속성 값 추가
    -Integer: 데이터 타입 Number 에서 Integer 로 변경
    -money: 초기 입력 금액

--- src/main/java/lotto/config/LottoGameConfig.java
+++ src/main/java/lotto/config/LottoGameConfig.java
@@ -4,4 +4,5 @@ import lotto.dto.Lotto;
 
 import java.util.List;
 
-public record LottoGameConfig(List<Lotto> lottos, List<Integer> winningNumbers, Number bonusNumber) { }
+public record LottoGameConfig(List<Lotto> lottos, List<Integer> winningNumbers, Integer bonusNumber, Integer money) {
+}
