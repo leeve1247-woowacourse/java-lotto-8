a3c269aee89f22a9e1c00d212780967fff1c1e3a
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-29 03:43:02 +0900

    refactor(dto): 게임 사전 설정값 dto 타입 record로 변경

--- src/main/java/lotto/config/LottoGameConfig.java
+++ src/main/java/lotto/config/LottoGameConfig.java
@@ -2,29 +2,6 @@ package lotto.config;
 
 import lotto.dto.Lotto;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class LottoGameConfig {
-    private List<Lotto> lottos = new ArrayList<>();
-    private List<Number> winningNumbers = new ArrayList<>();
-    private Number bonusNumber = null;
-
-    public LottoGameConfig(List<Lotto> lottos, List<Number> winningNumbers, Number bonusNumber) {
-        this.lottos = lottos;
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
-    }
-
-    public List<Lotto> getLottos() {
-        return lottos;
-    }
-
-    public List<Number> getWinningNumbers() {
-        return winningNumbers;
-    }
-
-    public Number getBonusNumber() {
-        return bonusNumber;
-    }
-}
+public record LottoGameConfig(List<Lotto> lottos, List<Integer> winningNumbers, Number bonusNumber) { }
