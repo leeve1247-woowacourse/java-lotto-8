a02223dd7de773e773c3b2de6f1f6a8ee6e1412e
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-29 03:41:49 +0900

    feat(config): 입력값 검증 클래스 추가

--- src/main/java/lotto/config/LottoGameConfigValidator.java
+++ src/main/java/lotto/config/LottoGameConfigValidator.java
@@ -1,4 +1,34 @@
 package lotto.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoGameConfigValidator {
+    public void check(String userInputMoney) throws IllegalArgumentException {
+        Integer money = Integer.valueOf(userInputMoney);
+        if (money < 1000) {
+            throw new IllegalArgumentException("금액이 부족합니다.");
+        }
+        if (money%1000 > 0){
+            throw new IllegalArgumentException("천 원 단위로 입력해주세요");
+        }
+    }
+
+    public void checkIfInRange(Integer a) throws IllegalArgumentException {
+        if (a < 1 || a > 45) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> checkAndParse(String winningNumbers) throws IllegalArgumentException {
+        return Arrays.stream(winningNumbers.split(",")).map(Integer::valueOf).peek(this::checkIfInRange).toList();
+    }
+
+    public void check(String rawBonusNumber, List<Integer> winningNumbers) {
+        Integer bonusNumber = Integer.valueOf(rawBonusNumber);
+        checkIfInRange(bonusNumber);
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
---
+++ src/test/java/lotto/config/LottoGameConfigValidatorTest.java
@@ -0,0 +1,31 @@
+package lotto.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SuppressWarnings("NonAsciiCharacters")
+class LottoGameConfigValidatorTest {
+    LottoGameConfigValidator lottoGameConfigValidator = new LottoGameConfigValidator();
+
+    @Test
+    void 구분자가_쉼표가_아님() {
+        assertThatThrownBy(() -> lottoGameConfigValidator
+                .checkAndParse("1-2-3-4-5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_범위를_벗어남() {
+        assertThatThrownBy(() -> lottoGameConfigValidator
+                .checkAndParse("1,2,3,-65,27"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 소수를_입력한_당첨번호() {
+        assertThatThrownBy(() -> lottoGameConfigValidator
+                .checkAndParse("1,2,3,3.5,27"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
