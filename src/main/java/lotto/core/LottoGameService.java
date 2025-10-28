4dc831836681bfd49e0ee4d564e313afe1a470c1
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-29 05:57:22 +0900

    feat(core): 로또 게임 기능 추가
    -play(): 티켓별 등수 판정 및 당첨금 합산, 결과 반환

--- src/main/java/lotto/core/LottoGameService.java
+++ src/main/java/lotto/core/LottoGameService.java
@@ -1,12 +1,45 @@
 package lotto.core;
 
 import lotto.config.LottoGameConfig;
+import lotto.dto.Lotto;
 import lotto.dto.LottoGameRecord;
+import lotto.dto.Rank;
+
+import java.util.*;
 
 public class LottoGameService {
+    LottoGameConfig lottoGameConfig;
+
     public LottoGameService(LottoGameConfig lottoGameConfig) {
+        this.lottoGameConfig = lottoGameConfig;
     }
     public LottoGameRecord play() {
-        return new LottoGameRecord();
+        List<Lotto> lottos = lottoGameConfig.lottos();
+        List<Integer> winningNumbers = lottoGameConfig.winningNumbers();
+        Integer bonusNumber = lottoGameConfig.bonusNumber();
+        Integer money = lottoGameConfig.money();
+
+        Map<Rank, Integer> winnersByRank = new HashMap<>();
+        for(Rank rank: Rank.values()) {
+            winnersByRank.put(rank, 0);
+        }
+
+        Integer profit = 0;
+        for (Lotto lotto : lottos) {
+            List<Integer> sharedNumbers = new ArrayList<>(winningNumbers);
+            sharedNumbers.retainAll(lotto.getNumbers());
+            int sharedNumberCount = sharedNumbers.size();
+            boolean containedBonusNumber = lotto.getNumbers().contains(bonusNumber);
+
+            for(Rank rank: Rank.values()) {
+                if (sharedNumberCount == rank.getCondition() && rank.checkBonus(containedBonusNumber)) {
+                    winnersByRank.merge(rank, 1, Integer::sum);
+                    profit += rank.getPrize();
+                }
+            }
+        }
+
+        Double profitRate = (double)profit/money*100;
+        return new LottoGameRecord(winnersByRank, profitRate);
     }
 }
