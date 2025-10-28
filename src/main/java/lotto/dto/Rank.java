58cac44bf12022182679e48c4cd89f4c425bf074
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-29 05:53:09 +0900

    feat(enum): 로또 등수별 등급 enum 추가
    - prize: 당첨금
    - condition: 일치 숫자 갯수
    - bonus: 보너스 숫자 존재 여부

---
+++ src/main/java/lotto/dto/Rank.java
@@ -0,0 +1,37 @@
+package lotto.dto;
+
+import static java.lang.Boolean.FALSE;
+
+public enum Rank {
+    FIRST(2000000000, 6, null),
+    SECOND(30000000, 5, true),
+    THIRD(1500000, 5, false),
+    FOURTH(50000, 4, null),
+    FIFTH(5000, 3, null),;
+
+    private final Integer prize;
+    private final Integer condition;
+    private final Boolean bonus;
+
+    Rank(Integer prize, Integer condition, Boolean bonus) {
+        this.prize = prize;
+        this.condition = condition;
+        this.bonus = bonus;
+    }
+
+    public Integer getPrize() {
+        return prize;
+    }
+
+    public Integer getCondition(){
+        return condition;
+    }
+
+    public Boolean checkBonus(boolean comp){
+        return bonus == null || bonus == comp;
+    }
+
+    public Boolean getBonus() {
+        return bonus;
+    }
+}
