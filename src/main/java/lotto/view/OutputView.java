7ccac44b73c890ba8822b1ebaec8a4dd23b1073b
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-10-29 06:36:46 +0900

    feat(view): 진행 순서별 출력 기능 추가
    - print(lottos):  로또 번호 표시
    - printLottosHeader(count): 발급한 로또 갯수 표시
    - print(gameRecord): 로또 당첨 결과 표시

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,52 @@
 package lotto.view;
 
+import lotto.dto.Lotto;
 import lotto.dto.LottoGameRecord;
+import lotto.dto.Rank;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public void print(LottoGameRecord gameRecord) {
+        for (Map.Entry<Rank, Integer> entry : reverseSort(gameRecord)) {
+            System.out.printf("%d개 일치", entry.getKey().getCondition());
+            printIfBonusMatchingRqeuired(entry);
+            System.out.printf(" (%,d원) - %d개\n", entry.getKey().getPrize(), entry.getValue());
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.", gameRecord.profitRate());
+    }
+
+    private ArrayList<Map.Entry<Rank, Integer>> reverseSort(LottoGameRecord gameRecord) {
+        Map<Rank, Integer> rankIntegerMap = gameRecord.winnersByRank();
+        ArrayList<Map.Entry<Rank, Integer>> entries = new ArrayList<>(rankIntegerMap.entrySet());
+        entries.sort(Map.Entry.comparingByKey(Comparator.reverseOrder()));
+        return entries;
+    }
+
+    private void printIfBonusMatchingRqeuired(Map.Entry<Rank, Integer> entry) {
+        if (bonusMatchingRequired(entry)) {
+            System.out.print(", 보너스 볼 일치");
+        }
+    }
+
+    private boolean bonusMatchingRequired(Map.Entry<Rank, Integer> entry) {
+        return entry.getKey().getBonus() == Boolean.TRUE;
     }
 
     public void print(String string) {
         System.out.println(string);
+    }
+
+    public void print(List<Lotto> lottos) {
+        lottos.forEach(lotto -> {
+            System.out.println(lotto.getNumbers());
+        });
+    }
+
+    public void printLottosHeader(int count) {
+        System.out.printf("%d개를 구매했습니다.\n", count);
     }
 }
